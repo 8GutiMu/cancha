c620ccd82cbfb69b05d4307e739e738b467992c6
Your Name <you@example.com>
2017-04-11 12:14:54 -0500

    funcion botones

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,41 @@
-console.log("si se relaciona")
+console.log("si se relaciona");
+
 var botonFutbol = document.getElementById('botonFutbol');
+var botonBasket = document.getElementById('botonBasket');
+var botonTennis = document.getElementById('botonTennis');
 
 botonFutbol.addEventListener("click",mostrarCanchaFutbol);
+botonBasket.addEventListener("click",mostrarCanchaBasket);
+botonTennis.addEventListener("click",mostrarCanchaTennis);
 
 function mostrarCanchaFutbol() {
-  var mostrarFutbol = document.getElementById("futbol");
-  mostrarFutbol.style.display = "block";
 
+    var mostrarFutbol = document.getElementById("futbol");
+    var mostrarBasket = document.getElementById("basket");
+    var mostrarTennis = document.getElementById("tennis");
+
+    mostrarBasket.style.display = "none";
+    mostrarTennis.style.display="none"
+    mostrarFutbol.style.display = "block";
+
+}
+
+function mostrarCanchaBasket(){
+
+    var mostrarBasket = document.getElementById("basket");
+    var mostrarFutbol = document.getElementById("futbol");
+    var mostrarTennis = document.getElementById("tennis");
+    mostrarBasket.style.display = "block";
+    mostrarFutbol.style.display = "none";
+    mostrarTennis.style.display="none"
+
+}
+
+function mostrarCanchaTennis() {
+  var mostrarTennis = document.getElementById("tennis");
+  var mostrarBasket = document.getElementById("basket");
+  var mostrarFutbol = document.getElementById("futbol");
+  mostrarBasket.style.display = "none";
+  mostrarFutbol.style.display = "none";
+  mostrarTennis.style.display="block"
 }
